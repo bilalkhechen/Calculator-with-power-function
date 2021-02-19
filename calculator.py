d63effd9981487198f888c163a64d0ea82c84b7b
bilalkhechen <77293874+bilalkhechen@users.noreply.github.com>
2021-02-19 00:53:52 -0500

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,28 +1,27 @@
+from colored import stylize, fg
+
+print(stylize("\n---- | Youtube Downloader | ----\n", fg("red")))
+
 def power(base_num, pow_num):
     result = 1
-    for index in range(pow_num):
+    for index in range(int(pow_num)):
         result = result * base_num
     return result
 
 op = input("please enter the operator: ")
+num1 = float(input("please enter your first number: "))
+num2 = float(input("please enter you second number: "))
 
-if op == "^":
-    pow1 = int(input("please enter your first power number: "))
-    pow2 = int(input("please enter your second power number: "))
-    print(power(pow1, pow2))
-else :
-
-    num1 = float(input("please enter your first number: "))
-    num2 = float(input("please enter you second number: "))
-
-    if op == "+":
-        print (num1 + num2)
-    elif op == "-":
-        print (num1 - num2)
-    elif op == "/":
-        print (num1 / num2)
-    elif op == "*":
-        print (num1 * num2)
+if op == "+":
+    print (num1 + num2)
+elif op == "-":
+    print (num1 - num2)
+elif op == "/":
+    print (num1 / num2)
+elif op == "*":
+    print (num1 * num2)
+elif op == "^":
+    print(power(num1, num2))
     
-    else:
-        print ("Invalid operation. Try something else.")
+else:
+    print(stylize("\n---- | Invalid operation. Try something else. | ----\n", fg("red")))
